80dfad26af5f775ceac2e980342554add7594a8a
Nico22F <nicofabbricatore22@gmail.com>
2024-09-23 09:16:21 +0200

    finito

--- ULIMATE-CRUD22F/Form1.Designer.cs
+++ ULIMATE-CRUD22F/Form1.Designer.cs
@@ -37,6 +37,11 @@
             label2 = new Label();
             textbox_name = new TextBox();
             textbox_cognome = new TextBox();
+            update_label = new Label();
+            textboxudateCOGNOME = new TextBox();
+            labelcognomeupdate = new Label();
+            textBoxupdateNOME = new TextBox();
+            label3 = new Label();
             SuspendLayout();
             // 
             // CREATE
@@ -83,15 +88,16 @@
             // 
             LISTBOX.FormattingEnabled = true;
             LISTBOX.ItemHeight = 15;
-            LISTBOX.Location = new Point(411, 12);
+            LISTBOX.Location = new Point(411, 14);
             LISTBOX.Name = "LISTBOX";
             LISTBOX.Size = new Size(377, 424);
             LISTBOX.TabIndex = 4;
+            LISTBOX.SelectedIndexChanged += LISTBOX_SelectedIndexChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(286, 56);
+            label1.Location = new Point(297, 25);
             label1.Name = "label1";
             label1.Size = new Size(40, 15);
             label1.TabIndex = 5;
@@ -100,7 +106,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(286, 242);
+            label2.Location = new Point(277, 303);
             label2.Name = "label2";
             label2.Size = new Size(60, 15);
             label2.TabIndex = 6;
@@ -108,23 +114,71 @@
             // 
             // textbox_name
             // 
-            textbox_name.Location = new Point(243, 78);
+            textbox_name.Location = new Point(246, 43);
             textbox_name.Name = "textbox_name";
             textbox_name.Size = new Size(130, 23);
             textbox_name.TabIndex = 7;
             // 
             // textbox_cognome
             // 
-            textbox_cognome.Location = new Point(243, 269);
+            textbox_cognome.Location = new Point(243, 336);
             textbox_cognome.Name = "textbox_cognome";
             textbox_cognome.Size = new Size(130, 23);
             textbox_cognome.TabIndex = 8;
+            // 
+            // update_label
+            // 
+            update_label.AutoSize = true;
+            update_label.Location = new Point(180, 210);
+            update_label.Name = "update_label";
+            update_label.Size = new Size(274, 15);
+            update_label.TabIndex = 9;
+            update_label.Text = "INSERISCI IL NOME  E COGNOME DA MODIFICARE";
+            // 
+            // textboxudateCOGNOME
+            // 
+            textboxudateCOGNOME.Location = new Point(243, 400);
+            textboxudateCOGNOME.Name = "textboxudateCOGNOME";
+            textboxudateCOGNOME.Size = new Size(130, 23);
+            textboxudateCOGNOME.TabIndex = 10;
+            // 
+            // labelcognomeupdate
+            // 
+            labelcognomeupdate.AutoSize = true;
+            labelcognomeupdate.Location = new Point(243, 382);
+            labelcognomeupdate.Name = "labelcognomeupdate";
+            labelcognomeupdate.Size = new Size(136, 15);
+            labelcognomeupdate.TabIndex = 11;
+            labelcognomeupdate.Text = "Cognome da modficare ";
+            // 
+            // textBoxupdateNOME
+            // 
+            textBoxupdateNOME.Location = new Point(243, 111);
+            textBoxupdateNOME.Name = "textBoxupdateNOME";
+            textBoxupdateNOME.Size = new Size(130, 23);
+            textBoxupdateNOME.TabIndex = 12;
+            textBoxupdateNOME.TextChanged += textBox1_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(257, 82);
+            label3.Name = "label3";
+            label3.Size = new Size(116, 15);
+            label3.TabIndex = 13;
+            label3.Text = "Nome da modificare";
+            label3.Click += label3_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(textBoxupdateNOME);
+            Controls.Add(labelcognomeupdate);
+            Controls.Add(textboxudateCOGNOME);
+            Controls.Add(update_label);
             Controls.Add(textbox_cognome);
             Controls.Add(textbox_name);
             Controls.Add(label2);
@@ -153,5 +207,10 @@
         private Label label2;
         private TextBox textbox_name;
         private TextBox textbox_cognome;
+        private Label update_label;
+        private TextBox textboxudateCOGNOME;
+        private Label labelcognomeupdate;
+        private TextBox textBoxupdateNOME;
+        private Label label3;
     }
 }
--- ULIMATE-CRUD22F/Form1.cs
+++ ULIMATE-CRUD22F/Form1.cs
@@ -6,46 +6,122 @@ namespace ULIMATE_CRUD22F
         {
             InitializeComponent();
         }
+        List<CRUD> lista = new List<CRUD>();
+
+
 
         // CREARE ELEMENTO
         private void CREATE_Click(object sender, EventArgs e)
         {
-            if (textbox_name.Text != null && textbox_cognome.Text != null) 
+            if (textbox_name.Text != null && textbox_cognome.Text != null)
             {
-                CRUD crud = new CRUD(textbox_name.Text, textbox_cognome.Text);
+                CRUD rud = new CRUD(textbox_name.Text, textbox_cognome.Text);
+                lista.Add(rud);
 
-                LISTBOX.Items.Add(crud);
+                LISTBOX.Items.Add(rud.toString());
             }
         }
 
         // AGGIUNGERE LISTA
         private void READ_Click(object sender, EventArgs e)
         {
+            StreamReader sr = new StreamReader("elenco.txt");
+            string stringa = sr.ReadLine();
 
+            // scrivo nella listbox gli elementi del file
+            while (stringa != null)
+            {
+                LISTBOX.Items.Add(stringa);
+
+                stringa = sr.ReadLine();
+            }
+
+            sr.Close();
         }
 
         // AGGIORNARE ELEMENTI
 
         private void UPDATE_Click(object sender, EventArgs e)
         {
+            string nome_da_cambiare = $"{textbox_name.Text}";
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (nome_da_cambiare == lista[i].Nome)
+                {
+                    LISTBOX.Items.Remove($"{textbox_name.Text};{textbox_cognome.Text}");
+
+                    // modifica lista
+
+                    lista[i].Nome = textBoxupdateNOME.Text;
+                    lista[i].Cogome = textboxudateCOGNOME.Text;
+
+                    // modifica listbox
+
+                    LISTBOX.Items.Add($"{textBoxupdateNOME.Text};{textboxudateCOGNOME.Text}");
+
+                    break;
+                }
+            }
 
         }
 
         // ELIMINARE ELEMETO
         private void DELETE_Click(object sender, EventArgs e)
         {
+            string nome_da_cambiare = $"{textbox_name.Text}";
 
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (nome_da_cambiare == lista[i].Nome)
+                {
+                    LISTBOX.Items.Remove($"{textbox_name.Text};{textbox_cognome.Text}");
+                    lista.Remove(lista[i]);
+
+                    break;
+                }
+            }
         }
 
         // CARICAMENTO PROGRAMMA
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // se durante l'apertura il file non esiste, allora lo crea
 
+            if (File.Exists("elenco.txt") == false)
+            {
+                StreamWriter sw = new StreamWriter("elenco.txt");
+                sw.Close();
+            }
+
+            update_label.Visible = false;
         }
 
         // SALVATAGGIO FILE
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("elenco.txt");
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                sw.WriteLine(lista[i].toString());
+            }
+
+            sw.Close();
+        }
+
+        private void LISTBOX_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
@@ -53,8 +129,20 @@ namespace ULIMATE_CRUD22F
 
     class CRUD
     {
-        private string nome { get; set; }
-        private string cogome { get; set; }
+        private string nome;
+        private string cogome;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Cogome
+        {
+            get { return cogome; }
+            set { cogome = value; }
+        }
 
         public CRUD(string nome, string cogome)
         {
@@ -62,13 +150,10 @@ namespace ULIMATE_CRUD22F
             this.cogome = cogome;
         }
 
-        private List<CRUD> lista { get; set; }
-
-
-        public void create(string nome,string cognome)
+        public string toString()
         {
-            this.nome=nome;
-            this.cogome=cognome;
+
+            return $"{nome};{cogome}";
         }
     }
 }
